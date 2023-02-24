71d8757d70874471ba6b780ccc63054e16f5570d
suhas0407 <118986814+suhas0407@users.noreply.github.com>
2023-02-24 09:12:57 +0530

    Add files via upload

---
+++ Display n Numbers by do while loop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+
+    cout<<"Enter n";
+    cin>>n;
+
+    do
+    {
+        cout<<i<<endl;
+        i++;
+
+    }
+    while(i<=n);
+    return 0;
+}
